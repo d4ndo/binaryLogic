13c55b1dd0c05513e7df1f19c96343d3f434e3ac
doerrhoefer <dando@gmx.de>
2013-12-02 13:00:54 +0100

    Added two boolean attributes to class binary. signed and littleEndian.
    Modified function binary and as.binary,to deal with those two attributes.
    
    print.binary is calling print.default. removeing attributes first.
    
    Added == operator for binary number comparison.

--- R/BinaryS3.R
+++ R/BinaryS3.R
@@ -3,14 +3,18 @@
 #' @description Create objects of type "binary" vector.
 #' @details No floating point supported.
 #' @usage binary(n)
-#' @param n length of vector.
+#' @param n length of vector. Number of bits
 #' @return a binary vector of length n
 #' @seealso base::as.logical , base::is.logical, base::as.integer base::raw
 #' @export
-binary <- function(n) {
-    x <- logical(length)
+binary <- function(n, signed=FALSE, littleEndian=FALSE) {
+    if(missing(n)) stop("n is missing.")
+    x <- logical(n)
     class(x) <- c("binary", class(x))
-    x
+    attr(x, "signed") <- signed
+    attr(x, "littleEndian") <- littleEndian
+    if (signed) { x <- fillBits(x) }
+    return(x)
 }
 
 #' as Binary Vector
@@ -18,12 +22,37 @@ binary <- function(n) {
 #' @description convert object to "binary".
 #' @usage as.binary(x)
 #' @param x object to convert.
+#' @param signed  TRUE or FALSE. Unsigned by default. (two's complement) 
+#' @param littleEndian if TRUE. Big Endian if FALSE.
 #' @return a binary vector.
 #' @seealso base::as.logical , base::is.logical, base::as.integer base::raw
 #' @export
-as.binary <- function(x){
-    if(!inherits(x, "binary")) class(x) <- c("binary", class(x))
-    x
+as.binary <- function(x, signed=FALSE, littleEndian=FALSE){
+    if(!inherits(x, "binary")) {
+        class(x) <- c("binary", class(x))
+        attr(x, "signed") <- signed
+        attr(x, "littleEndian") <- littleEndian
+        if (signed) { x <- fillBits(x) }        
+    } else {
+        if (signed) {
+            attr(x, "signed") <- TRUE
+            x <- fillBits(x)
+        } else {
+            attr(x, "signed") <- FALSE
+        }
+        if (littleEndian) {
+            if (!attributes(x)$littleEndian)
+            {
+                switchEndianess(x)
+            }
+        } else {
+            if (attributes(x)$littleEndian)
+            {
+                switchEndianess(x)
+            }
+        }
+    }
+    return(x)
 }
 
 #' is Binary Vector
@@ -35,14 +64,27 @@ as.binary <- function(x){
 #' @seealso base::as.logical , base::is.logical, base::as.integer base::raw
 #' @export
 is.binary <- function(x) {
-    inherits(x, "binary")
+    return(inherits(x, "binary"))
 }
 
 print.binary <- function(x,...) {
     x <- ifelse(x, as.integer(1), as.integer(0))
-    cat(x,"\n")
-    #NextMethod(x,...)
-    #cat("PRI")
+    attributes(x) <- NULL
+    print.default(x,...)
+}
+
+'==.binary' <- function(x,y) {
+    if(length(x) >= length(y)) {
+        MAX <- length(x)
+        delta <- length(x) - length(y)
+        fillBits(y, delta)
+    } else {
+        MAX <- length(y)
+        delta <- length(y) - length(x)
+        fillBits(x, delta)
+    }
+
+    return(all(!xor(x,y)))
 }
 
 #'+.bin' <- function(x,y) {
