a2553363f3abc2489b0ce0ad6037ed95f3924d7a
doerrhoefer <dando@gmx.de>
2013-12-05 15:50:50 +0100

    fixed the binAdd function. some problems with negative numbers.

--- R/binAdd.R
+++ R/binAdd.R
@@ -8,6 +8,8 @@
 #' @param y summand 2 (binary vector)
 #' @return The sum of x and y. Returns a binary vector.
 #' @examples
+#' five <- dec2bin(5); ten <- dec2bin(10);
+#' bin2dec(binAdd(ten, five))
 #' binAdd(as.binary(c(0,1)),as.binary(c(1,0)))
 #' @seealso base::as.logical , base::is.logical, base::raw
 #' @export
@@ -33,21 +35,19 @@ binAdd <- function(x, y) {
         a <- rep(FALSE,length(y)-length(x))
         x <- as.binary(c(a,x), signed=x_signed, littleEndian=x_littleEndian)
     }
-    
     ret = binary(MAX)
     temp = binary(MAX+1)
     ret[MAX] <- xor(x[MAX],y[MAX])
-    if (isTRUE(x[MAX]) & isTRUE(y[MAX])) temp[MAX+1] = TRUE
-    
+    if ((isTRUE(as.logical(x[MAX])) & isTRUE(as.logical(y[MAX])))) temp[MAX+1] <- TRUE
     if (MAX > 2)
     {
         for(i in (MAX-1):1)
         {
             ret[i] <- xor(x[i],y[i])
             ret[i] <- xor(ret[i],temp[i+2])
-            if((isTRUE(x[i]) & isTRUE(y[i])) | 
-                   (isTRUE(x[i]) & isTRUE(temp[i+2])) | 
-                   (isTRUE(y[i]) & isTRUE(temp[i+2]))) temp[i+1] = TRUE
+            if(((isTRUE(as.logical(x[i])) & isTRUE(as.logical(y[i]))) | 
+                   (isTRUE(as.logical(x[i])) & isTRUE(as.logical(temp[i+2]))) | 
+                   (isTRUE(as.logical(y[i])) & isTRUE(as.logical(temp[i+2]))))) temp[i+1] <- TRUE
         }
     }
     if (temp[2] & !signed) ret <- as.binary(c(T,ret), signed=TRUE, littleEndian=FALSE)
