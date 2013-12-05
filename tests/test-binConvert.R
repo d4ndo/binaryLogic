a3cbddeafe6070d5d4d6873219e4d73961bc9dc3
doerrhoefer <dando@gmx.de>
2013-12-05 15:50:01 +0100

    Added a comment

--- tests/test-binConvert.R
+++ tests/test-binConvert.R
@@ -10,7 +10,7 @@ test_that("Return value", {
     expect_that(dec2bin(0, littleEndian=TRUE, signed=TRUE, size=1), is_equivalent_to(as.binary(c(0,0,0,0,0,0,0,0))))
     expect_that(dec2bin(1), is_equivalent_to(as.binary(c(1))))
     expect_that(dec2bin(1, signed=TRUE, size=1), is_equivalent_to(as.binary(c(0,0,0,0,0,0,0,1))))
-    expect_that(dec2bin(1, littleEndian=TRUE, signed=TRUE, size=1), is_equivalent_to(as.binary(c(1,0,0,0,0,0,0,0))))    
+    expect_that(dec2bin(1, littleEndian=TRUE, signed=TRUE, size=1), is_equivalent_to(as.binary(c(1,0,0,0,0,0,0,0))))
     expect_that(dec2bin(-1,signed=TRUE), is_equivalent_to(as.binary(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1))))
     expect_that(dec2bin(-1, littleEndian=TRUE, signed=TRUE, size=2), is_equivalent_to(as.binary(c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1))))
     expect_that(dec2bin(8, signed=TRUE, size=1) , is_equivalent_to(as.binary(c(0,0,0,0,1,0,0,0))))
@@ -22,6 +22,8 @@ test_that("Warnings", {
     expect_that(dec2bin(-2), throws_error("Negative number is not possible with unsigned method."))
     #expect_that(dec2bin(2^15, signed=TRUE), throws_error("Out of Range. Please increase the size[Byte]"))
 })
+
+# need some testing for attributes
 
 context("bin2dec")
 
