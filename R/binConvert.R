f42aaf6f22fc4914982c177bc82a88c3cae73775
doerrhoefer <dando@gmx.de>
2013-12-08 20:16:08 +0100

    Added links @seealso. itemized detail information.

--- R/binConvert.R
+++ R/binConvert.R
@@ -4,8 +4,10 @@
 #' @details The binary number is represented by a logical vector.
 #' The Bit order usually follows the same endianness as the byte order.
 #' No floating-point support.
-#' Little Endian    (LSB) ---> (MSB)
-#' Big Endian       (MSB) <--- (LSB)
+#' \itemize{
+#' \item Little Endian    (LSB) ---> (MSB)
+#' \item Big Endian       (MSB) <--- (LSB)
+#' }
 #' @usage dec2bin(num, signed=FALSE, littleEndian=FALSE, size=2)
 #' @param num     integer (hex) or (decimal) number.
 #' @param signed  TRUE or FALSE. Unsigned by default. (two's complement) 
@@ -16,7 +18,7 @@
 #' dec2bin(0xfa)
 #' dec2bin(3)
 #' dec2bin(-1, signed=TRUE, size=1)
-#' @seealso binaryLogic::as.binary, binaryLogic::is.binary, base::as.logical, base::is.logical, base::raw
+#' @seealso \link{bin2dec} and \link{binary} or \link{logical}
 #' @export
 dec2bin <- function(num, signed=FALSE, littleEndian=FALSE, size=2) {
     if (missing(num)) stop("num is missing.")
@@ -56,8 +58,10 @@ dec2bin <- function(num, signed=FALSE, littleEndian=FALSE, size=2) {
 #' 
 #' @description Converts a binary number to a decimal or hex number.
 #' @details No floating-point support.
-#' Little Endian    (LSB) ---> (MSB)
-#' Big Endian       (MSB) <--- (LSB)
+#' \itemize{
+#' \item Little Endian    (LSB) ---> (MSB)
+#' \item Big Endian       (MSB) <--- (LSB)
+#' }
 #' @usage bin2dec(bin, hex=FALSE)
 #' @param bin      binary number. Any logical or binary vector.
 #' @param hex      TRUE or FALSE: if TRUE it returns an hex value. by default = FALSE
@@ -66,7 +70,7 @@ dec2bin <- function(num, signed=FALSE, littleEndian=FALSE, size=2) {
 #' bin2dec(as.binary(c(1,0,1,1,1,0,1,1)))
 #' bin2dec(as.binary(c(TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE)))
 #' bin2dec(as.binary(c(TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE)), hex=TRUE)
-#' @seealso binaryLogic::as.binary, binaryLogic::is.binary, base::as.logical, base::is.logical, base::raw
+#' @seealso \link{dec2bin} and \link{binary} or \link{logical}
 #' @export
 bin2dec <- function(bin, hex=FALSE) {
     if (missing(bin)) stop("bin is missing.")
