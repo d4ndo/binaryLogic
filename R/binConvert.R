66b063a6136c2416558c46cbf592de032072af29
doerrhoefer <dando@gmx.de>
2013-12-20 01:00:07 +0100

    rewrite function dec2bin(). To avoid the modulo problem. Maxium num now is 2^1016 (127 Bye). Converting a value < 0 is still very slow (see binAdd). Auto switching signed flag if num < 0.

--- R/binConvert.R
+++ R/binConvert.R
@@ -8,12 +8,13 @@
 #' \item Little Endian    (LSB) ---> (MSB)
 #' \item Big Endian       (MSB) <--- (LSB)
 #' }
+#' Auto switch to signed if num < 0.
 #' @usage dec2bin(num, signed=FALSE, littleEndian=FALSE, size=2)
 #' @param num     integer (hex) or (decimal) number.
 #' @param signed  TRUE or FALSE. Unsigned by default. (two's complement) 
 #' @param littleEndian if TRUE. Big Endian if FALSE.
-#' @param size    Needed if signed is set. (by default 2 Byte)
-#' @return The binary number. Returns a binary and logical vector.
+#' @param size in Byte. Needed if »signed« is set. (by default 2 Byte)
+#' @return The binary number. Returns a (binary/logical) vector.
 #' @examples
 #' dec2bin(0xfa)
 #' dec2bin(3)
@@ -21,38 +22,52 @@
 #' @seealso \link{bin2dec} and \link{binary} or \link{logical}
 #' @export
 dec2bin <- function(num, signed=FALSE, littleEndian=FALSE, size=2) {
-    #should be implemened in C. Very slow with negative numbers. (binAdd)
-    #Increase limit of num to 8 Byte. modulo has problems with num > 2^54.
-    if (num < 0 && !signed) stop("Negative number is not possible with unsigned method.")
-    if (signed && (((num > ((2^(size*Byte())/2)-1))) || (num < ((-1)*(2^(size*Byte())/2))))) {
-        stop("Out of Range. Please increase the size[Byte]")
-    }
-
-    size <- size * Byte()
+    # Very slow with negative numbers. (maybe binAdd)
+    if (signed && (((num > ((2^(size*Byte())/2)-1))) || 
+                    (num < ((-1)*(2^(size*Byte())/2)))))
+    stop("Out of Range. Please increase the size[Byte]")
+    
+    l <- list(class=c("binary","logical"),
+              signed=signed,
+              littleEndian=littleEndian)
+    
+    #needed for hex input
+    num <- as.numeric(num)
+    
     neg = FALSE
-    if(num < 0) neg = TRUE
-    num = abs(num)
-    rest <- num %% 2
+    if (num < 0) {
+        l$signed <- TRUE
+        neg = TRUE
+        num = abs(num)
+    }
+    
+    #global read only for function h(num)
+    ret <- h(num)
 
-    while((num <- num %/% 2) != 0)
+    if(l$signed) b <- logical(size*Byte()) else b <- logical(max(ret)-1)
+    #do some optimization here.
+    for (i in seq(length(b))) b[i] <- any(ifelse((ret-1)==i, TRUE, FALSE))
+    
+    if (neg)
     {
-        rest <- c(num %% 2, rest)
+        b <- !rev(b)
+        b <- rev(binAdd(as.binary(b, signed=TRUE), as.binary(TRUE)))
     }
-    if (signed)
-    {
-        a <- rep(TRUE, size)
-        b <- rep(FALSE, size - length(rest))
-        ret <- c(b, rest)
-        if (neg)
-        {
-            ret <- !ret
-            ret <- binAdd(as.binary(ret, signed=TRUE), as.binary(TRUE))
-        }
-        if(littleEndian) ret <- rev(ret)
-        return(as.binary(ret, signed=signed, littleEndian=littleEndian))
+    if(!l$littleEndian) b <- rev(b)
+    loadAttributes(b, l)
+    return(loadAttributes(b, l))
+}
+
+# Helper function to convert decimal to binary
+h <- function(x) {
+    # v should be global.
+    v <- c(0,  2^(0:1016))
+    ret <- numeric(1)
+    for(i in 1:length(v)) {
+        if (v[i] > x) { ret <- c(i-1, h(x-v[i-1])); break }
+        if (v[i] == x) return(ret <- c(i, ret))
     }
-    if(littleEndian) rest <- rev(rest)
-    return(as.binary(rest, signed=signed, littleEndian=littleEndian))
+    return(ret)
 }
 
 #' Converts a binary number to a decimal or hex number.
@@ -66,7 +81,7 @@ dec2bin <- function(num, signed=FALSE, littleEndian=FALSE, size=2) {
 #' @usage bin2dec(bin, hex=FALSE)
 #' @param bin      binary number. Any logical or binary vector.
 #' @param hex      TRUE or FALSE: if TRUE it returns an hex value. by default = FALSE
-#' @return The decimal or hex number »integer«.
+#' @return The decimal or hex number »numeric«.
 #' @examples
 #' bin2dec(as.binary(c(1,0,1,1,1,0,1,1)))
 #' bin2dec(as.binary(c(TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE)))
