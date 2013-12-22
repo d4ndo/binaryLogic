ea385664d2ac8f569cc7070093e87d0db4e6414f
doerrhoefer <dando@gmx.de>
2013-12-22 19:20:17 +0100

    New tests for +.binary , -.binary, ==.binary and !=.binary.
    binAdd using save and loadAttributes instead as.binary.

--- R/binAdd.R
+++ R/binAdd.R
@@ -1,7 +1,7 @@
 #' Binary Addition (+)
 #' 
 #' @description Adds two binary numbers. (x + y)
-#' @details Little-Endian is not supported at the moment. No floating point supported.
+#' @details Little-Endian and unsigned is not supported at the moment. No floating point supported.
 #' if x or y is signed the return value will also be signed.
 #' @usage binAdd(x, y)
 #' @param x summand 1 (binary vector)
@@ -19,27 +19,36 @@ binAdd <- function(x, y) {
     if (missing(y)) stop("y is missing.")
     stopifnot(is.binary(x))
     stopifnot(is.binary(y))
+    
     x_signed <- attributes(x)$signed
     x_littleEndian <- attributes(x)$littleEndian
     y_signed <- attributes(y)$signed
     y_littleEndian <- attributes(y)$littleEndian
-    signed <- FALSE
-    if (x_signed || y_signed) signed <- TRUE
+    
+    l <- list(class=c("binary","logical"),
+              signed=FALSE,
+              littleEndian=x_littleEndian)
+    
+    if (x_signed || y_signed) l$signed <- TRUE
+    if (x_littleEndian) y <- switchEndianess(y)
+    if (y_littleEndian) x <- switchEndianess(x)    
     
     if (length(x) >= length(y))
     {
         MAX <- length(x)
         a <- rep(FALSE,length(x)-length(y))
-        y <- as.binary(c(a,y), signed=y_signed, littleEndian=y_littleEndian)
+        y <- c(a,y)
     } else {
         MAX <- length(y)
         a <- rep(FALSE,length(y)-length(x))
-        x <- as.binary(c(a,x), signed=x_signed, littleEndian=x_littleEndian)
+        x <- c(a,x)
     }
+    
     ret = binary(MAX)
     temp = binary(MAX+1)
     ret[MAX] <- xor(x[MAX],y[MAX])
     if ((isTRUE(as.logical(x[MAX])) && isTRUE(as.logical(y[MAX])))) temp[MAX+1] <- TRUE
+    
     if (MAX > 2)
     {
         for(i in (MAX-1):1)
@@ -51,6 +60,11 @@ binAdd <- function(x, y) {
                    (isTRUE(as.logical(y[i])) && isTRUE(as.logical(temp[i+2]))))) temp[i+1] <- TRUE
         }
     }
-    if (temp[2] && !signed) ret <- as.binary(c(T,ret), signed=TRUE, littleEndian=FALSE)
-    return(ret)
+    if (temp[2] && !l$signed) {
+        ret <- c(T,ret)
+        #l$signed <- TRUE
+        #l$littleEndian <- FALSE
+    }
+    # reverse big endian.
+    return(loadAttributes(ret, l))
 }
