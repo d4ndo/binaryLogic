38549d54573b75fb3fe828f3eb16bcfff1ad56d7
Daniel Dörrhöfer <pcre@gmx.de>
2016-06-23 11:09:41 +0200

    Added devision %/% and modulo %% to the operator table.

--- operator.R
+++ operator.R
@@ -1,7 +1,7 @@
-Operator <- c("[== or !=]", "[&lt;, &lt;= or > , >=]", "[+ or -]", "[*, ^]", "[/]", "[&, &#166;, xor]", "[!]")
-Behavior <- c("Comparision by value.", "Comparision by value.", "Operations by value.", "Operations by value.", " Not supported.", "Bitwise Operations. The smaller vector is filled up  with zeros.", "Indicates logical negation (NOT). Bitwise Operations")
+Operator <- c("[== or !=]", "[&lt;, &lt;= or > , >=]", "[+ or -]", "[*, ^]", "[%/%, %%]","[/]", "[&, &#166;, xor]", "[!]")
+Behavior <- c("Comparision by value.", "Comparision by value.", "Operations by value.", "Operations by value.", "Operations by value", " Not supported.", "Bitwise Operations. The smaller vector is filled up  with zeros.", "Indicates logical negation (NOT). Bitwise Operations")
 op <- as.data.frame(cbind(Operator,Behavior))
 
-Operator <- c("shiftLeft(binary), shiftRight(binary)", "rotate(binary)", "negate(binary)", "switchEndianess(binary)")
-Behavior <- c("shift Operation.", "shift Operation.", "Indicates arithmetic negation. value <- value * (-1)", " ")
+Operator <- c("shiftLeft(binary), shiftRight(binary)", "rotate(binary)", "negate(binary)", "switchEndianess(binary)", "bin2gray(binary)", "gray2bin(gray)")
+Behavior <- c("shift Operation.", "shift Operation.", "Indicates arithmetic negation. value <- value * (-1)", " ", "convert binary to gray code", "convert gray code to binary")
 op2 <- as.data.frame(cbind(Operator,Behavior))
